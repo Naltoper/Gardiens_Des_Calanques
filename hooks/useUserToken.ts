bc2b240068421bb3332f09fdc6788b011dedeb0e
FABRETTI Florent <florent.fabretti@etu.univ-amu.fr>
2026-04-30 13:31:37 +0200

    refactor: improve user token management and platform handling

--- hooks/useUserToken.ts
+++ hooks/useUserToken.ts
@@ -1,35 +1,15 @@
-import * as Crypto from 'expo-crypto';
-import * as SecureStore from 'expo-secure-store';
 import { useEffect, useState } from 'react';
-import { Platform } from 'react-native';
+import { getUserToken } from '../utils/storage';
 
 export function useUserToken() {
   const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
-    const getToken = async () => {
-      let storedToken = null;
-
-      if (Platform.OS === 'web') {
-        storedToken = localStorage.getItem('user_token');
-      } else {
-        storedToken = await SecureStore.getItemAsync('user_token');
-      }
-
-      if (!storedToken) {
-        storedToken = Crypto.randomUUID();
-
-        if (Platform.OS === 'web') {
-          localStorage.setItem('user_token', storedToken);
-        } else {
-          await SecureStore.setItemAsync('user_token', storedToken);
-        }
-      }
-
-      setToken(storedToken);
+    const initToken = async () => {
+      const validToken = await getUserToken();
+      setToken(validToken);
     };
-
-    getToken();
+    initToken();
   }, []);
 
   return token;
